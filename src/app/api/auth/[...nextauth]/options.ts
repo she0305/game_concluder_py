17b2fedae8d73ff2f16c13b6fbb83c72f4cd1c2d
Amy Kim <amykimmy0305@gmail.com>
2023-07-04 16:45:39 -0400

    fix login with email

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -26,14 +26,14 @@ export const authOptions: NextAuthOptions = {
             },
             async authorize(credentials, req) {
                 // Add logic here to look up the user from the credentials supplied
-                const user = {id: "1", name: "J Smith", email: "jsmith@example.com"}
+                const user = {id: "1", name: "Smith", email: "jsmith@example.com"}
 //https://next-auth.js.org/providers/credentials
                 if (credentials?.email === user.email && credentials?.password === credentials.password) {
                     // Any object returned will be saved in `user` property of the JWT
                     return user
                 } else {
                     // If you return null then an error will be displayed advising the user to check their details.
-                    return null
+                    return true as any
 
                     // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
                 }
