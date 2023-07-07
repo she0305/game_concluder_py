124581d7aa2470d2e2d0b3f96f5ee10a828b122c
Amy Kim <amykimmy0305@gmail.com>
2023-07-07 00:16:01 -0400

    fix extension issue caused by grammarly

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" className={sans.className}>
-        <body className="flex flex-col w-full max-w-screen-3xl mx-auto">
+        <body className="flex flex-col w-full max-w-screen-3xl mx-auto" suppressHydrationWarning={true} >
         <AuthContext>
             <header>
                 <Navbar/>
